31b53f2b109a43e86c6e9d2b70538d0a4b75ac88
Sergey <cydoor88@gmail.com>
2023-06-26 19:56:25 +0300

    add documents view

--- PmHelper/ClientApp/src/components/Header.vue
+++ PmHelper/ClientApp/src/components/Header.vue
@@ -9,20 +9,31 @@
         </div>
 
         <div class="inline-flex gap-6 justify-end items-center">
-            <router-link
-                v-if="!isAuthenticated"
-                to="/login"
-                class="items-center px-4 py-2 border border-transparent text-base font-medium rounded-md text-white bg-gray-700 hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
-            >
-                Sign In
-            </router-link>
-            <router-link
-                v-else
-                to="/profile"
-                class="items-center px-4 py-2 border border-transparent text-base font-medium rounded-md text-white bg-gray-700 hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
-            >
-                Profile
-            </router-link>
+            <div>
+                <router-link
+                    to="/documents"
+                    class="items-center px-4 py-2 border border-transparent text-base font-medium rounded-md text-white bg-gray-700 hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+                >
+                    Documents
+                </router-link>
+            </div>
+
+            <div>
+                <router-link
+                    v-if="!isAuthenticated"
+                    to="/login"
+                    class="items-center px-4 py-2 border border-transparent text-base font-medium rounded-md text-white bg-gray-700 hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+                >
+                    Sign In
+                </router-link>
+                <router-link
+                    v-else
+                    to="/profile"
+                    class="items-center px-4 py-2 border border-transparent text-base font-medium rounded-md text-white bg-gray-700 hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+                >
+                    Profile
+                </router-link>
+            </div>
         </div>
     </nav>
 </template>
--- PmHelper/ClientApp/src/views/Documents.vue
+++ PmHelper/ClientApp/src/views/Documents.vue
@@ -1,5 +1,75 @@
 <template>
-    <div class="flex flex-col justify-center items-center">
-        Documents
+    <div class="flex flex-col justify-center items-center mx-5 my-5">
+        <!--
+            https://ui.vuestic.dev/ui-elements/card
+        -->
+
+        <div class="grid gap-4 grid-cols-3">
+            <!--
+                <va-card
+                    href="https://vuestic.epicmax.co/"
+                    target="_blank"
+                >
+            -->
+            <va-card
+                color="primary"
+                gradient
+            >
+                <va-card-title>Project Charter</va-card-title>
+                <va-card-content>
+                    Project Charter is a detailed document that outlines the goals, stakeholders, scope and other relevant information regarding a project.
+                </va-card-content>
+                <img src="@/assets/images/documents/project-charter.webp" class="h-54 bg-base-100" alt="Project charter" />
+            </va-card>
+
+            <va-card
+                color="primary"
+                gradient
+                :bordered="false"
+            >
+                <va-card-title>App Requirements</va-card-title>
+                <va-card-content>
+                    App Requirements is a detailed document that outlines the user personas, features, and functionality of a software application.
+                </va-card-content>
+                <img src="@/assets/images/documents/app-req.webp" class="mt-2 h-54 bg-base-100" alt="" />
+            </va-card>
+
+            <va-card
+                color="primary"
+                gradient
+            >
+                <va-card-title>Feature Requirements</va-card-title>
+                <va-card-content>
+                    Contains everything the development team needs to start working on a new feature.
+                </va-card-content>
+                <img src="@/assets/images/documents/feature-req.webp" class="mt-2 h-54 bg-base-100" alt="" />
+            </va-card>
+
+            <va-card
+                color="primary"
+                gradient
+            >
+                <va-card-title>User story</va-card-title>
+                <va-card-content>
+                    User story descrciption
+                </va-card-content>
+                <va-image
+                    src="@/assets/images/documents/feature-req.webp"
+                    class="h-56"
+                />
+            </va-card>
+
+            <!--TO DO CARD-->
+            <va-card
+                color="secondary"
+                stripe
+                stripe-color="success"
+            >
+                <va-card-title>Title</va-card-title>
+                <va-card-content>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                </va-card-content>
+            </va-card>
+        </div>
     </div>
 </template>
