073831a082d01482c2452c44e05d9a4a7f081e22
Sergey <cydoor88@gmail.com>
2023-07-13 19:15:32 +0300

    add env to chat gpt proxy

--- ChatGptProxy/config.js
+++ ChatGptProxy/config.js
@@ -3,28 +3,29 @@ export const APP_NAME = "SChatGpt Proxy";
 export const VERSION = "0.9.5";
 
 // SET DEBUG
-export const DEBUG = true; // Debug mode
+export const DEBUG = process.env.DEBUG_MODE; // Debug mode
 
 // Server configuration
-export const SERVER_PORT = 3000; // Server port
+export const SERVER_PORT = process.env.SERVER_PORT; // Server port
 
 // Rate limit
-export const RATE_PERIOD = 15 * 1000; // 15 seconds
-export const RATE_LIMIT = 50; // 50 requests per 15 seconds
+export const RATE_PERIOD = process.env.RATE_PERIOD; // 15 seconds
+export const RATE_LIMIT = process.env.RATE_LIMIT; // 50 requests per 15 seconds
 
 // Completions URLS
 export const COMPLETIONS_URL = "https://api.openai.com/v1/completions";
 export const CHAT_COMPLETIONS_URL = "https://api.openai.com/v1/chat/completions";
 
 // Whitelisted IPs
-export const USE_WHITELISTED_IPS = true;
-export const WHITELISTED_IPS = [
+export const USE_WHITELISTED_IPS = process.env.USE_WHITELISTED_IPS;
+export const WHITELISTED_IPS = process.env.WHITELISTED_IPS.split(',');
+/*export const WHITELISTED_IPS = [
     "127.0.0.1",
     "::1",
-];
+];*/
 
 // Prompt Moderation before sending to OpenAI
-export const MODERATION = true; // Moderation mode
+export const MODERATION = process.env.MODERATION; // Moderation mode
 
 // OpenAI API Keys
 export let OPENAI_KEY = process.env.OPENAI_KEY;
